a84268c9eaac1ebc1ad35a6447ca4ecf017c2ec7
Jared Griggs <griggsjared@gmail.com>
2025-04-04 19:51:25 -0600

    chore: configure assets directory and remove generator meta tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwind from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    assets: '_assets'
+  },
   vite: {
     plugins: [
       tailwind(),
